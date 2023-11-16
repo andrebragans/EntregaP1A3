32974f99e6903b972cc8655bbb3c0bed48ab184f
André Neves dos Santos Braga <38845628+andrebragans@users.noreply.github.com>
2023-11-16 17:20:08 -0300

    Delete src/gameslevel directory

--- src/gameslevel/javacomlog.js
+++
@@ -1,9 +0,0 @@
-
-function perfil() {
-    
-  
-    alert("Redirecionando para tela do seu perfil!");
-
-    document.getElementById("meuFormulario").action = "telaperfil.html";
-    document.getElementById("meuFormulario").submit();      
-}
